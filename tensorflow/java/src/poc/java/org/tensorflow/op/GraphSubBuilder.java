82f268c7e7faf086488a00603d0016e0dc17277d
Karl Lessard <karl@kubx.ca>
2017-05-07 09:02:11 -0400

    Small fix

--- tensorflow/java/src/poc/java/org/tensorflow/op/GraphSubBuilder.java
+++ tensorflow/java/src/poc/java/org/tensorflow/op/GraphSubBuilder.java
@@ -1,15 +1,13 @@
 package org.tensorflow.op;
 
-import org.tensorflow.Graph;
-
 class GraphSubBuilder extends GraphBuilder {
 
-  GraphSubBuilder(Graph graph, Scope scope) {
+  GraphSubBuilder(Graph2 graph, Scope scope) {
     super(graph, scope);
   }
   
   @Override
-  public Graph build() {
+  public Graph2 build() {
     throw new UnsupportedOperationException("Graph subbuilders cannot finalize the graph");
   }
 }
